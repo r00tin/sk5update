b80c13d8b4c435276d5863e398b6e4d70be5fc3b
r00tin <106145127+r00tin@users.noreply.github.com>
2022-06-06 23:18:19 +0800

    Update sk5duo.sh

--- sk5duo.sh
+++ sk5duo.sh
@@ -1,63 +1,98 @@
 #!/bin/bash
-# mir4 sk多端口补丁
-# Author: 0.0<https://www.baidu.com>>
-service sockd stop
-v=`ip a  |grep inet |grep -v inet6 |grep -v '127.0.0.1' |awk '{print $2}' |awk -F / '{print$1}'`
-echo $v
-echo "# Generate by sockd.info" > /etc/danted/sockd.conf
-echo "# Generate interface $v" >> /etc/danted/sockd.conf
-echo "internal: $v  port = 8080" >> /etc/danted/sockd.conf
-echo "internal: $v  port = 8001" >> /etc/danted/sockd.conf
-echo "internal: $v  port = 8002" >> /etc/danted/sockd.conf
-echo "internal: $v  port = 8003" >> /etc/danted/sockd.conf
-echo "internal: $v  port = 8004" >> /etc/danted/sockd.conf
-echo "internal: $v  port = 8005" >> /etc/danted/sockd.conf
-echo "internal: $v  port = 8006" >> /etc/danted/sockd.conf
-echo "internal: $v  port = 8007" >> /etc/danted/sockd.conf
-echo "internal: $v  port = 8008" >> /etc/danted/sockd.conf
-echo "internal: $v  port = 8009" >> /etc/danted/sockd.conf
-echo "internal: $v  port = 8010" >> /etc/danted/sockd.conf
-echo "internal: $v  port = 8011" >> /etc/danted/sockd.conf
-echo "internal: $v  port = 8012" >> /etc/danted/sockd.conf
-echo "internal: $v  port = 8013" >> /etc/danted/sockd.conf
-echo "internal: $v  port = 8014" >> /etc/danted/sockd.conf
-echo "internal: $v  port = 8015" >> /etc/danted/sockd.conf
-echo "internal: $v  port = 8016" >> /etc/danted/sockd.conf
-echo "internal: $v  port = 8017" >> /etc/danted/sockd.conf
-echo "internal: $v  port = 8018" >> /etc/danted/sockd.conf
-echo "internal: $v  port = 8019" >> /etc/danted/sockd.conf
-echo "internal: $v  port = 8020" >> /etc/danted/sockd.conf
-echo "external: $v" >> /etc/danted/sockd.conf
+install_http() {
+    
+  yum install -y squid #安装http代理
+  cat <<EOF >/etc/squid/squid.conf
+#
+# Recommended minimum configuration:
+#
+acl manager proto cache_object
+acl localhost src 127.0.0.1/32 ::1
+acl to_localhost dst 127.0.0.0/8 0.0.0.0/32 ::1
 
-echo "method: pam none" >> /etc/danted/sockd.conf
-echo "clientmethod: none" >> /etc/danted/sockd.conf
-echo "user.privileged: root" >> /etc/danted/sockd.conf
-echo "user.notprivileged: sockd" >> /etc/danted/sockd.conf
-echo "logoutput: /var/log/sockd.log" >> /etc/danted/sockd.conf
+# Example rule allowing access from your local networks.
+# Adapt to list your (internal) IP networks from where browsing
+# should be allowed
+acl localnet src 10.0.0.0/8	# RFC1918 possible internal network
+acl localnet src 172.16.0.0/12	# RFC1918 possible internal network
+acl localnet src 192.168.0.0/16	# RFC1918 possible internal network
+acl localnet src fc00::/7       # RFC 4193 local private network range
+acl localnet src fe80::/10      # RFC 4291 link-local (directly plugged) machines
 
-echo "client pass {" >> /etc/danted/sockd.conf
-echo "        from: 0.0.0.0/0  to: 0.0.0.0/0" >> /etc/danted/sockd.conf
-echo "}" >> /etc/danted/sockd.conf
-echo "client block {" >> /etc/danted/sockd.conf
-echo "        from: 0.0.0.0/0 to: 0.0.0.0/0" >> /etc/danted/sockd.conf
-echo "}" >> /etc/danted/sockd.conf
-echo "pass {" >> /etc/danted/sockd.conf
-echo "        from: 0.0.0.0/0 to: 0.0.0.0/0" >> /etc/danted/sockd.conf
-echo "        protocol: tcp udp" >> /etc/danted/sockd.conf
-echo "        method: pam" >> /etc/danted/sockd.conf
-echo "        log: connect disconnect" >> /etc/danted/sockd.conf
-echo "}" >> /etc/danted/sockd.conf
-echo "block {" >> /etc/danted/sockd.conf
-echo "        from: 0.0.0.0/0 to: 0.0.0.0/0" >> /etc/danted/sockd.conf
-echo "        log: connect error" >> /etc/danted/sockd.conf
-echo "}" >> /etc/danted/sockd.conf
-service sockd adduser 112233 112233
-service sockd start
-clear
-echo -e "\033[33m正在后台工作，清等待：\033[0m"
-for i in $(seq 10 -1 1)
-do
-    echo -e "$i/s";sleep 1
-done
-echo -e "\033[33m处理完毕\n本脚本由一指年华提供\n已添加8011-8020端口\n账号密码112233\n脚本退出\033[0m"
- 
+acl SSL_ports port 443
+acl Safe_ports port 80		# http
+acl Safe_ports port 21		# ftp
+acl Safe_ports port 443		# https
+acl Safe_ports port 70		# gopher
+acl Safe_ports port 210		# wais
+acl Safe_ports port 1025-65535	# unregistered ports
+acl Safe_ports port 280		# http-mgmt
+acl Safe_ports port 488		# gss-http
+acl Safe_ports port 591		# filemaker
+acl Safe_ports port 777		# multiling http
+acl CONNECT method CONNECT
+
+#
+# Recommended minimum Access Permission configuration:
+#
+# Only allow cachemgr access from localhost
+http_access allow manager localhost
+http_access deny manager
+
+# Deny requests to certain unsafe ports
+http_access deny !Safe_ports
+
+# Deny CONNECT to other than secure SSL ports
+http_access deny CONNECT !SSL_ports
+
+# We strongly recommend the following be uncommented to protect innocent
+# web applications running on the proxy server who think the only
+# one who can access services on "localhost" is a local user
+#http_access deny to_localhost
+
+#
+# INSERT YOUR OWN RULE(S) HERE TO ALLOW ACCESS FROM YOUR CLIENTS
+#
+
+# Example rule allowing access from your local networks.
+# Adapt localnet in the ACL section to list your (internal) IP networks
+# from where browsing should be allowed
+http_access allow localnet
+#http_access allow localhost
+
+# And finally deny all other access to this proxy
+#http_access deny all
+http_access allow all
+
+# Squid normally listens to port 3128
+#http_port 3128
+http_port 59494
+via off
+forwarded_for delete
+
+# We recommend you to use at least the following line.
+hierarchy_stoplist cgi-bin ?
+
+# Uncomment and adjust the following to add a disk cache directory.
+#cache_dir ufs /var/spool/squid 100 16 256
+
+# Leave coredumps in the first cache dir
+coredump_dir /var/spool/squid
+
+# Add any of your own refresh_pattern entries above these.
+refresh_pattern ^ftp:		1440	20%	10080
+refresh_pattern ^gopher:	1440	0%	1440
+refresh_pattern -i (/cgi-bin/|\?) 0	0%	0
+refresh_pattern .		0	20%	4320
+EOF
+  systemctl start squid          #开启squid
+  systemctl restart squid          #开启squid
+  systemctl enable squid.service #设置开机自动启动
+}
+install_socks5() {
+    
+  wget --no-check-certificate https://raw.github.com/Lozy/danted/master/install.sh -O install_proxy.sh
+  bash install_proxy.sh --port=10010 --user=mir001 --passwd=mir001
+}
+install_http
+install_socks5
