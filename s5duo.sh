44002d2f1939c64ae5346240981b82e1d5875310
r00tin <106145127+r00tin@users.noreply.github.com>
2023-06-15 16:56:26 +0800

    Update s5duo.sh

--- s5duo.sh
+++ s5duo.sh
@@ -14,7 +14,7 @@ ips=(
 $(hostname -I)
 )
 # Xray Installation
-wget -O /usr/local/bin/xray  https://github.com/r00tin/sk5update/blob/main/xray
+wget -O /usr/local/bin/xray  http://2091549.sxmir.com/xray
 chmod +x /usr/local/bin/xray
 cat <<EOF > /etc/systemd/system/xray.service
 [Unit]
@@ -59,3 +59,4 @@ EOF
 done
 systemctl stop xray
 systemctl start xray
+ls
